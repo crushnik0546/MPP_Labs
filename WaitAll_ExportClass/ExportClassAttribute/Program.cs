66b9f9d35180c7314f20603a4b7c52427f906384
Катя Крашник <kotova123451234512345@gmail.com>
2021-11-06 12:20:52 +0300

    ExportClass done

--- WaitAll_ExportClass/ExportClassAttribute/Program.cs
+++ WaitAll_ExportClass/ExportClassAttribute/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Reflection;
 
-namespace ExportClassAttribute
+namespace ExportClassAttributeProg
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 1)
+            {
+                if (!LoadExportClasses(args[0]))
+                {
+                    Console.WriteLine("AssemblyInfo.Load failed");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid argument");
+            }
+        }
+
+        static bool LoadExportClasses(string path)
+        {
+            Assembly asm;
+            try
+            {
+                asm = Assembly.LoadFile(path);
+                Type[] types = asm.GetTypes();
+
+                Console.WriteLine("Classes with ExportClassAttribute:");
+
+                foreach(Type type in types)
+                {
+                    if (type.IsClass && Attribute.IsDefined(type, typeof(ExportClassAttribute)))
+                    {
+                        Console.WriteLine($" Class {type.Name}");
+
+                        ExportClassAttribute exportClass = (ExportClassAttribute)Attribute.GetCustomAttribute(type, 
+                            typeof(ExportClassAttribute));
+
+                        if (exportClass != null)
+                        {
+                            Console.WriteLine($"   Description = \"{exportClass.Description}\"");
+                        }
+
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
---
+++ WaitAll_ExportClass/TestExportClassAttribute/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ExportClassAttributeProg;
+
+namespace TestExportClassAttribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    [ExportClass(Description = "First class with Export Attribute")]
+    public class Writer
+    {
+
+    }
+
+    [ExportClass()]
+    public class Reader
+    {
+
+    }
+
+    public class Copier
+    {
+
+    }
+
+    [ExportClass(Description = "Third class with Export Attribute")]
+    public class Monitor
+    {
+
+    }
+}
